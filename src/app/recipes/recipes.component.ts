14bef6b555bdbc159efa7d5ec899eae6267e6510
ajrobbins44 <rob16003@byui.edu>
2024-09-26 12:53:59 -0700

    update

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.css'
+  styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent {
-
+  constructor() {}
+  ngOnInit(): void {
+      
+  }
 }
