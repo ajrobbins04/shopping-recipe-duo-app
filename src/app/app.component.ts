2129e16bd8c475f2f44f93ee8efd8eea61b1d87e
ajrobbins44 <rob16003@byui.edu>
2024-11-07 17:47:11 -0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-recipe-duo-app';
-  loadedFeature = 'recipe';
-  
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
+
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Injectable } from '@angular/core';
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
 @Injectable()
@@ -12,17 +11,11 @@ import { RecipeService } from './recipe.service';
 })
 
 export class RecipesComponent implements OnInit{
-  selectedRecipe: Recipe;
   
-  constructor(private recipeService: RecipeService) {
+  constructor() {
   }
 
   ngOnInit() { 
-    this.recipeService.recipeSelected
-      .subscribe(
-        (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-        }
-      );
+
   }
 }
